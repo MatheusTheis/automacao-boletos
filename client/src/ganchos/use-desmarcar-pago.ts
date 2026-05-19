c26798f241267e6a824b8549e42663ee55ccf003
Matheus Theis <matheus.theis@hotmail.com>
2026-05-18 22:33:09 -0300

    Revert "testerevert"
    This reverts commit 05c2992646fe85e1c5a1198b1490ba277b0688c8.

--- client/src/ganchos/use-desmarcar-pago.ts
+++ client/src/ganchos/use-desmarcar-pago.ts
@@ -14,7 +14,7 @@ export function useDesmarcarPago() {
       const resposta = await fetch(montarUrlApi('/api/boletos/desmarcar-pago'), {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',//testerevert
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           nossoNumero: boleto.nossoNumero,
